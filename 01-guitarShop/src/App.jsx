19b33409910895ee7d3ff1a4fe35be6c0d852c20
guido2288 <guido2288@gmail.com>
2025-09-14 20:37:28 -0300

    01-guitarShop Project added

---
+++ 01-guitarShop/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import Guitar from './components/Guitar'
+import Header from './components/Header'
+import { db } from './data/db';
+
+
+function App() {
+
+  const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = ( item ) => {
+    const itemExist = cart.findIndex( (guitar) => guitar.id === item.id );
+    if( itemExist >= 0) {
+      const updatedCart = [...cart];
+      updatedCart[itemExist].quantity++;
+      setCart(updatedCart);
+    } else{
+      item.quantity = 1;
+      setCart(  [...cart, item] );
+    }
+  }
+
+  return (
+    <>
+      <Header />
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {
+            data.map( (guitar) => (
+              <Guitar
+                guitar={guitar}
+                key={guitar.id}
+                addToCart={addToCart}
+              />
+            ) )
+          }
+        </div>
+      </main>
+
+
+      <footer className="bg-dark mt-5 py-5">
+        <div className="container-xl">
+          <p className="text-white text-center fs-4 mt-4 m-md-0">GuitarLA - Todos los derechos Reservados</p>
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default App
